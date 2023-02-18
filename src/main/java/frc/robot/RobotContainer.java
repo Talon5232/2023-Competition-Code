d625477003644a9e4398f9cfee72cb53e20db157
Talon5232 <61335902+Talon5232@users.noreply.github.com>
2023-02-18 17:54:37 -0600

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,51 +4,21 @@ import java.sql.DriverAction;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.autos.*;
-import frc.robot.commands.*;
-import frc.robot.subsystems.*;
-
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
-public class RobotContainer {
+@@ -25,8 +26,7 @@ public class RobotContainer {
     /* Controllers */
     private final XboxController xcontrollers = new XboxController(0);
     private final Joystick driver = new Joystick(0);
+    //private final Joystick driver2 = new Joystick(1);
+
     private final Joystick driver2 = new Joystick(1);
 
     /* Drive Controls */
     private final int translationAxis = XboxController.Axis.kLeftY.value;
-    private final int strafeAxis = XboxController.Axis.kLeftX.value;
-    private final int rotationAxis = XboxController.Axis.kRightX.value;
-    
-    
-    
-
-    /* Driver Buttons */
-    private final JoystickButton zeroGyro = new JoystickButton(driver, XboxController.Button.kY.value);
-    private final JoystickButton bbutton = new JoystickButton(driver, XboxController.Button.kB.value);
-    private final JoystickButton abutton = new JoystickButton(driver, XboxController.Button.kA.value);
-    private final JoystickButton xbutton = new JoystickButton(driver, XboxController.Button.kX.value);
-    private final JoystickButton Startbutton = new JoystickButton(driver, XboxController.Button.kStart.value);
-    private final JoystickButton backButton = new JoystickButton(driver, XboxController.Button.kBack.value);
-
-
-    private final JoystickButton leftBumper = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
+@@ -49,15 +49,15 @@ public class RobotContainer {
     private final JoystickButton rightBumper = new JoystickButton(driver, XboxController.Button.kRightBumper.value);
     private final JoystickButton robotCentric = new JoystickButton(driver, XboxController.Button.kLeftBumper.value);
     //Thrust Master Buttons
+    //private final boolean trigger = driver2.getRawButton(1);
     private final boolean trigger = driver2.getRawButton(1);
 
     /* Subsystems */
@@ -58,32 +28,15 @@ public class RobotContainer {
     private final Swerve s_Swerve = new Swerve();
     private final intakeSub m_Intake = new intakeSub();
 
+
     /** The container for the robot. Contains subsystems, OI devices, and commands. */
     public RobotContainer() {
         s_Swerve.setDefaultCommand(
-            new TeleopSwerve(
-                s_Swerve, 
-                () -> -driver.getRawAxis(translationAxis), 
-                () -> -driver.getRawAxis(strafeAxis), 
-                () -> -driver.getRawAxis(rotationAxis), 
-                () -> robotCentric.getAsBoolean()
-            )
-        );
-
-        // Configure the button bindings
-        configureButtonBindings();
-    }
-
-    /**
-     * Use this method to define your button->command mappings. Buttons can be created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-     */
-    private void configureButtonBindings() {
+@@ -84,26 +84,23 @@ private void configureButtonBindings() {
         /* Driver Buttons */
-        
+
         abutton.whileTrue(new InstantCommand(() -> m_arm.armUp())).whileFalse(new InstantCommand(() -> m_arm.armStop()));
+       // backButton.whileTrue(new InstantCommand(() -> m_arm.armMovement(double setpoint + .025)));
         abutton.whileTrue(new InstantCommand(() -> m_arm.armMovement(0))).whileFalse(new InstantCommand(() -> m_arm.armStop()));
 
         bbutton.whileTrue(new InstantCommand(() -> m_arm.armDown())).whileFalse(new InstantCommand(() -> m_arm.armStop()));
@@ -91,7 +44,19 @@ public class RobotContainer {
         rightBumper.whileTrue(new InstantCommand(() -> m_lift.liftDown())).whileFalse(new InstantCommand(() -> m_lift.liftStop()));
         xbutton.whileTrue(new InstantCommand(() -> m_Intake.intakeDown())).whileFalse(new InstantCommand(() -> m_Intake.intakeStop()));
         Startbutton.whileTrue(new InstantCommand(() -> m_Intake.intakeUp())).whileFalse(new InstantCommand(() -> m_Intake.intakeStop()));
-    
+        /* 
+        if(trigger == true){
+           new InstantCommand(() -> m_lift.liftUp());
+        }
+        else{
+            new InstantCommand(() -> m_lift.liftDown());
+        }
+        */
+
+
+
+
+        zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
 
     if(trigger == true){
         new InstantCommand(() -> m_lift.liftUp());
@@ -100,15 +65,7 @@ public class RobotContainer {
     }
     zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
 }
-    
+
 
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
-     *
-     * @return the command to run in autonomous
-     */
-    public Command getAutonomousCommand() {
-        // An ExampleCommand will run in autonomous
-        return new exampleAuto(s_Swerve);
-    }
-}
