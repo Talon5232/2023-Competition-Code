aab7f789360af8fee0bbdb7ee2789bb32a5b066b
Talon5232 <61335902+Talon5232@users.noreply.github.com>
2023-03-19 11:49:52 -0500

    Update blinkin.java

--- src/main/java/frc/robot/subsystems/blinkin.java
+++ src/main/java/frc/robot/subsystems/blinkin.java
@@ -1,17 +1,8 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved. */
-/* Open Source Software - may be modified and shared by FRC teams. The code */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project. */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
-
-    import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
-    import edu.wpi.first.wpilibj2.command.SubsystemBase;
-    import frc.robot.RobotContainer;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class blinkin extends SubsystemBase {
 
@@ -34,13 +25,5 @@ public void givecone() {
     blinkin.set(-.07); 
     
 }
- /*   public static void lightsclose() {
-        if (25>Ultrasonic){
-            blinkin.set(0.77);
-        }
-    }
-    public static void lightscloseish() {
-        if (30>Ultrasonic>15){
-            blinkin.set(0.69);
-        }} */
+
 }
